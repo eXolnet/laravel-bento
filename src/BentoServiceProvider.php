4ff8c3309751a57c030f78de730683b629a451e7
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2017-08-05 11:20:28 -0400

    Move Blade directives registration to the boot method

--- src/BentoServiceProvider.php
+++ src/BentoServiceProvider.php
@@ -1,7 +1,7 @@
 <?php namespace Exolnet\Bento;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\View\Compilers\BladeCompiler;
 
 class BentoServiceProvider extends ServiceProvider
 {
@@ -13,6 +13,30 @@ class BentoServiceProvider extends ServiceProvider
     protected $defer = false;
 
     /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Blade::directive('launch', function ($expression) {
+            return "<?php if (\Exolnet\Bento\BentoFacade::launch($expression)): ?>";
+        });
+
+        Blade::directive('endlaunch', function () {
+            return '<?php endif; ?>';
+        });
+
+        Blade::directive('unlesslaunch', function ($expression) {
+            return "<?php if ( ! \Exolnet\Bento\BentoFacade::launch($expression)): ?>";
+        });
+
+        Blade::directive('endunlesslaunch', function () {
+            return '<?php endif; ?>';
+        });
+    }
+
+    /**
      * Register the service provider.
      *
      * @return void
@@ -20,33 +44,5 @@ class BentoServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->singleton('bento', Bento::class);
-
-        $this->registerBladeDirectives();
-    }
-
-    /**
-     * Register Blade directives.
-     *
-     * @return void
-     */
-    protected function registerBladeDirectives()
-    {
-        $this->app->afterResolving('blade.compiler', function (BladeCompiler $bladeCompiler) {
-            $bladeCompiler->directive('launch', function ($expression) {
-                return "<?php if (\Exolnet\Bento\BentoFacade::launch($expression)): ?>";
-            });
-
-            $bladeCompiler->directive('endlaunch', function () {
-                return '<?php endif; ?>';
-            });
-
-            $bladeCompiler->directive('unlesslaunch', function ($expression) {
-                return "<?php if ( ! \Exolnet\Bento\BentoFacade::launch($expression)): ?>";
-            });
-
-            $bladeCompiler->directive('endunlesslaunch', function () {
-                return '<?php endif; ?>';
-            });
-        });
     }
 }

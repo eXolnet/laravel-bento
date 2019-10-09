2baf38feb4509af00df7958f060a4802e1f07271
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2019-10-09 09:24:44 -0400

    Add a middleware that returns a 404 if the specified feature isn't launched

---
+++ src/Middleware/Feature.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Exolnet\Bento\Middleware;
+
+use Closure;
+use Exolnet\Bento\Bento;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class Feature
+{
+    /**
+     * @var \Exolnet\Bento\Bento
+     */
+    protected $bento;
+
+    /**
+     * @param \Exolnet\Bento\Bento $bento
+     */
+    public function __construct(Bento $bento)
+    {
+        $this->bento = $bento;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @param string $feature
+     * @return mixed
+     */
+    public function handle($request, Closure $next, string $feature)
+    {
+        if (! $this->bento->launch($feature)) {
+            throw new NotFoundHttpException;
+        }
+
+        return $next($request);
+    }
+}
---
+++ tests/Unit/Middleware/FeatureTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Exolnet\Bento\Tests\Unit\Middleware;
+
+use Exolnet\Bento\Bento;
+use Exolnet\Bento\Middleware\Feature;
+use Exolnet\Bento\Tests\UnitTest;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Mockery as m;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class FeatureTest extends UnitTest
+{
+    /**
+     * @var \Exolnet\Bento\Bento|\Mockery\MockInterface
+     */
+    protected $bento;
+
+    /**
+     * @var \Illuminate\Http\Request|\Mockery\MockInterface
+     */
+    protected $request;
+
+    /**
+     * @var \Exolnet\Bento\Middleware\Feature
+     */
+    protected $middleware;
+
+    /**
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->bento = m::mock(Bento::class);
+        $this->request = m::mock(Request::class);
+
+        $this->middleware = new Feature($this->bento);
+    }
+
+    /**
+     * @return void
+     */
+    public function testFeatureLaunched(): void
+    {
+        $this->bento->shouldReceive('launch')->once()->with('feature')->andReturn(true);
+
+        $responseExpected = m::mock(Response::class);
+
+        $next = function (...$arguments) use ($responseExpected) {
+            $this->assertCount(1, $arguments);
+            $this->assertEquals($this->request, $arguments[0]);
+
+            return $responseExpected;
+        };
+
+        $responseActual = $this->middleware->handle($this->request, $next, 'feature');
+
+        $this->assertEquals($responseExpected, $responseActual);
+    }
+
+    /**
+     * @return void
+     */
+    public function testFeatureNotLaunched(): void
+    {
+        $this->bento->shouldReceive('launch')->once()->with('feature')->andReturn(false);
+
+        $next = function () {
+            // The next method should not be called
+            $this->assertFalse(true);
+        };
+
+        $this->expectException(NotFoundHttpException::class);
+
+        $this->middleware->handle($this->request, $next, 'feature');
+    }
+}

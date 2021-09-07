04f76a4b5b53c042d861fc0b18894f4ea8fc0da4
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2021-09-07 16:19:19 -0400

    feat: add type hints to the Bento facade

--- src/Facades/Bento.php
+++ src/Facades/Bento.php
@@ -6,6 +6,12 @@ use Illuminate\Support\Facades\Facade;
 
 /**
  * @see \Exolnet\Bento\Bento
+ *
+ * @method static \Exolnet\Bento\Feature feature(string $name)
+ * @method static bool launch(string $name)
+ * @method static bool await(string $name)
+ * @method static int getVisitorId()
+ * @method static \Exolnet\Bento\Bento setVisitorId(int $visitorId)
  */
 class Bento extends Facade
 {
